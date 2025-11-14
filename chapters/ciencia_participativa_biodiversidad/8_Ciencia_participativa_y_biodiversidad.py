68a0064f15dcb221c3b32f9235ec868c73746f35
Nelson Salinas <nrsalinas@gmail.com>
2025-11-14 10:02:52 -0500

    Ciencia participativa

--- chapters/ciencia_participativa_biodiversidad/8_Ciencia_participativa_y_biodiversidad.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-import pandas as pd
-import geopandas as gpd
-import plotly.express as px
-
--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ pg = st.navigation(
 		st.Page("chapters/flora/2_Flora_de_Bogotá.py", url_path="flora"), 
 		st.Page("chapters/plantas_urbanos/3_Plantas_de_ecosistemas_urbanos_y_periurbanos.py", url_path="ecosistemas_urbanos"), 	
 		st.Page("chapters/avifauna/5_Avifauna_de_Bogotá.py", url_path="aves"),
+		st.Page("chapters/ciencia_participativa_biodiversidad/8_Biodiversidad_y_ciencia_participativa.py", url_path="biodiv_ciencia_part"),
 		st.Page("chapters/interacciones/9_Interacciones_bióticas.py", url_path="interacciones")
 	]
 )
