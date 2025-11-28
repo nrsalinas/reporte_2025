88317882d5e1b403d3854f8568b471a1f85c3428
Nelson Salinas <nrsalinas@gmail.com>
2025-11-28 12:03:27 -0500

    Hojarasca updated.

--- chapters/suelos/11_Hojarasca_y_suelos.py
+++ chapters/suelos/11_Hojarasca_y_suelos.py
@@ -27,8 +27,9 @@ st.markdown("""
 
 #### Jardín Botánico de Bogotá, eje Conservación _in situ_
 			
-Bogotá tiene suelos!
-""")
+En áreas verdes urbanas, el suelo además de ser un refugio de biodiversidad es un aliado estratégico para la mitigación del cambio climático. En este estudio se analizó la variación del aporte de hojarasca fina, el flujo de CO<sub>2</sub> y algunas propiedades del suelo (carbono, materia orgánica, pH y densidad aparente) en tres áreas de importancia ecológica de Bogotá (El Jardín Botánico de Bogotá, Las Mercedes y el Bosque Urbano de Ciudad Montes - BUCM). El Jardín Botánico exhibió un mayor aporte de hojarasca (4.17 t ha<sup>-1</sup>) y flujo de CO<sub>2</sub> (2.77 µmol m<sup>-2</sup> s<sup>-1</sup>), lo que sugiere mejor actividad biológica y respiratoria en el suelo. En las Mercedes se presentaron valores intermedios de hojarasca y CO<sub>2</sub> del suelo; mientras que el BUCM presentó los valores más bajos en aporte de hojarasca (1.49 t ha<sup>-1</sup>) y flujo de CO<sub>2</sub> (1.56 µmol m<sup>-2</sup> s<sup>-1</sup>). En relación con las propiedades del suelo, el BUCM exhibió un valor alto de carbono orgánico (180.2 t ha<sup>-1</sup>), sugiriendo un alto potencial de secuestro de carbono pese a que la densidad aparente refleja compactación y baja aireación del suelo. Estos hallazgos evidencian que las áreas verdes urbanas pueden actuar como sumideros de carbono, especialmente cuando se implementan prácticas de conservación del suelo, como mantener la cobertura de hojarasca.
+			
+""", unsafe_allow_html=True)
 
 with st.container(border=True, horizontal_alignment='center'):
 
@@ -39,7 +40,7 @@ with st.container(border=True, horizontal_alignment='center'):
 		color="Componente de hojarasca",
 		barmode='group',
 		title='Composición de la hojarasca',
-		#color_discrete_sequence=['Bisque', 'DarkRed', 'Coral']
+		color_discrete_sequence=['OrangeRed', 'DarkRed', 'LightSalmon', 'Bisque']
 	)
 
 	#bar = bar.update_layout(height=500)
@@ -53,7 +54,7 @@ with st.container(border=True, horizontal_alignment='center'):
 		y="Sitio",
 		#color="Flujo CO2 (microl m2 s-1)",
 		title='Flujo de CO2 en el suelo',
-		#color_discrete_sequence=['Bisque', 'DarkRed', 'Coral']
+		color_discrete_sequence=['DarkRed']
 	)
 
 	#bar1 = bar1.update_layout(height=500)
@@ -74,7 +75,8 @@ with st.container(border=True, horizontal_alignment='center'):
 		lon='Longitud',
 		hover_name = 'Sitio',
 		hover_data={'Size':False, 'Latitud':False, 'Longitud':False},
-		color_continuous_scale=px.colors.cyclical.IceFire, 
+		#color_continuous_scale=px.colors.cyclical.IceFire, 
+		color_discrete_sequence=['OrangeRed'],
 		zoom=10,
 		size="Size",
 		height=800
