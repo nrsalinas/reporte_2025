fbd0b7b3c388acc061fabcaf071769b3daa68497
Nelson Salinas <nrsalinas@gmail.com>
2025-12-04 14:23:28 -0500

    Mapas coberturas

--- chapters/coberturas/1_Coberturas.py
+++ chapters/coberturas/1_Coberturas.py
@@ -3,8 +3,8 @@ import pandas as pd
 import geopandas as gpd
 import plotly.express as px
 
-cob_ori_file = "chapters/coberturas/dat/Ecosistemas_originaless_v7.shp"
-cob_act_file = "chapters/coberturas/dat/Ecosistemas_actuales_v5.shp"
+cob_ori_file = "chapters/coberturas/dat/Ecosistemas_originaless_v7_light.shp"
+cob_act_file = "chapters/coberturas/dat/Ecosistemas_actuales_v5_light.shp"
 logo = "shared/figs/Color.png"
 
 ###  Map data
@@ -26,9 +26,10 @@ cob_act = cob_act.to_crs(4326)
 
 with st.container(border=True, horizontal_alignment='center'):
 
+	st.markdown("**Coberturas vegetales originales**")
+
 	fig = px.choropleth_map(
 		cob_ori,
-		title='Número de especies por localidad',
 		geojson=cob_ori.geometry,
 		locations=cob_ori.index,
 		hover_name="Eco1",
@@ -47,6 +48,65 @@ with st.container(border=True, horizontal_alignment='center'):
 			'Gray', #Territorio Artificializado 
 			'LightSeaGreen', #Turbera                    
 			'DodgerBlue', #Zona Pantanosa 
+		],
+		opacity=0.7,
+	)
+
+	fig.update_geos(
+		fitbounds="geojson", 
+		visible=False,
+		bgcolor='rgba(0,0,0,0)',
+		framewidth=3,
+		)
+
+	fig.update_layout(
+		margin={"r":0,"t":0,"l":0,"b":0},
+		paper_bgcolor='rgba(0,0,0,0)',
+		height=1300,
+		map=dict(
+			center={"lat":ctr_lat, "lon":ctr_lon},
+			zoom=9.5,
+		),
+		showlegend=False,
+		coloraxis_showscale=False
+	)
+
+	st.plotly_chart(fig, use_container_width=True)
+
+st.markdown("""#""")
+
+left_co, cent_co,last_co = st.columns(3)
+with cent_co:
+	st.image(logo)
+
+with st.container(border=True, horizontal_alignment='center'):
+
+	st.markdown("**Coberturas vegetales actuales**")
+
+	fig = px.choropleth_map(
+		cob_act,
+		geojson=cob_act.geometry,
+		locations=cob_act.index,
+		hover_name="ecos_sinte",
+		color="ecos_sinte",
+		labels={"ecos_sinte": "Ecosistema actual"},
+		color_discrete_sequence=[
+			'LightSteelBlue', # Agroecosistema            
+			'Olive', # Arbustal                  
+			'Green', # Bosque                    
+			'DarkSeaGreen', # Bosque Fragmentado        
+			'Black', # Complejos Rocosos         
+			'DarkCyan', # Cuerpo de Agua Artificial 
+			'DarkSeaGreen', # Herbazal                  
+			'Blue', # Laguna                    
+			'SaddleBrown', # Paramo                    
+			'Teal', # Rio                       
+			'OrangeRed', # Subxerofitia              
+			'Gray', # Territorio Artificializado
+			'LightSlateGrey', # Transicional Transformado 
+			'LightSeaGreen', # Turbera                   
+			'LightGreen', # Vegetacion Secundaria     
+			'DodgerBlue', # Zona Pantanosa            
 		],
 		opacity=0.7,
 	)
